1921d18a315c863503867437572298d468b3157a
grazielac <cpgraziela@gmail.com>
2025-09-08 22:27:01 +0100

    javascript attempt w/o peaking

---
+++ app.js
@@ -0,0 +1,14 @@
+let task = [];
+
+function writeTask() {
+    const input = document.getElementById("one");
+    input.addEventListener("click", writeTask);
+}
+
+function addButton(){
+    const addTask = document.getElementById("butt");
+    addTask.addEventListener("click", ;
+}
+
+const input = document.getElementById("one");
+const taskOne = 
