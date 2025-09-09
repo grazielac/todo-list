222a0f751eb7ede394f768aef410cc25fbe4a941
grazielac <cpgraziela@gmail.com>
2025-09-09 19:28:24 +0100

    removed # from html input element id

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 let task = [];
 
-function writeTask() {
-    const input = document.getElementById("one");
-    input.addEventListener("click", writeTask);
-}
-
-function addButton(){
-    const addTask = document.getElementById("butt");
-    addTask.addEventListener("click", ;
-}
-
 const input = document.getElementById("one");
-const taskOne = 
+const button = document.getElementById("myButton");
+const taskContainer = document.getElementsByTagName("ul");
+
+button.addEventListener("click", function (event) {
+  // this codes listen for a click on the button.
+  document.getElementById("one");
+  const inputValue = input.value; // why input.value
+  console.log(inputValue);
+
+  console.log(event);
+});
